48c7d07207cdc7287d16136cc17b565887470a13
IlyaChekin13 <ilya.chekin13@gmail.com>
2022-02-12 20:05:59 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -2,6 +2,9 @@ import requests
 import sys
 import os
 import re
+from bs4 import BeautifulSoup
+from colorama import Fore, init, Style
+init()
 regex_url = r'[https?://www\.]?.*\..*'
 
 
@@ -11,11 +14,45 @@ class Browser:
         self.arg = args[1]
         self.hist = []
 
+    @staticmethod
+    def cheking_url(inp):
+        """add 'https://' if it's not included"""
+        if not inp[:8] == "https://":
+            return "https://" + inp
+        else:
+            return inp
+
+    @staticmethod
+    def req_parsing(url):
+        """get request, pars content, and highlight the output"""
+        req = requests.get(url)
+        tags = ['p', 'a', 'ul', 'ol', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+        soup = BeautifulSoup(req.content, 'html.parser').body.descendants
+        page = ""
+        for descendant in soup:
+            if descendant.name in tags:
+                if descendant.name == 'a':
+                    page += Fore.BLUE + descendant.get_text().strip()
+                else:
+                    page += Style.RESET_ALL + descendant.get_text().strip()
+        return page
+
+
+
+
     def create_dir(self):
         """Create the directory with saved pages"""
         if not os.access(self.arg, os.F_OK):
             os.mkdir(self.arg)
         os.chdir(self.arg)
+
+    def back(self):
+        """return previous page"""
+        try:
+            print(self.hist.pop(-2))
+#           looks like cheating with stack :)
+        except IndexError:
+            pass
 
     def go(self):
         """main algorithm"""
@@ -25,10 +62,10 @@ class Browser:
             url = self.cheking_url(user_inp)
             file_path = self.arg + "\\" + user_inp
             if re.findall(regex_url, url):
-                req = requests.get(url)
-                print(req.text)
+                print(self.req_parsing(url))
                 with open(user_inp, "w") as f:
-                    f.write(req.text)
+                    f.write(self.req_parsing(url))
+                    self.hist.append(self.req_parsing(url))
             elif os.path.isfile(file_path):
                 with open(user_inp) as f:
                     print(f.read())
@@ -37,19 +74,7 @@ class Browser:
             elif user_inp == "exit":
                 break
             else:
-                print("URL Error")
-
-    @staticmethod
-    def cheking_url(inp):
-        if not inp[:8] == "https://":
-            return "https://" + inp
-
-    def back(self):
-        try:
-            print(self.hist.pop(-2))
-#           looks like cheating with stack :)
-        except IndexError:
-            pass
+                print("Incorrect URL")
 
 
 def main():
